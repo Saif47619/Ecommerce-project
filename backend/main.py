f2f7dd85117d8995547ce0a1af94d58f8d53f6a4
Saif47619 <saifullahnajeeb47@gmail.com>
2026-07-03 14:47:40 +0500

    Setup PostgreSQL and FASTAPI connection

--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,26 @@ from models import Base, Product
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
-
+@app.get("/")
+def root():
+    return {"message": "API Running"}
 @app.post("/products")
 def create_product():
     db = SessionLocal()
+@app.get("/products")
+def get_products():
+    db = SessionLocal()
 
+    products = db.query(Product).all()
+
+    return [
+        {
+            "id": p.id,
+            "name": p.name,
+            "price": p.price
+        }
+        for p in products
+    ]
     new_product = Product(
         name="Shirt",
         price=20
@@ -24,3 +39,6 @@ def create_product():
         "name": new_product.name,
         "price": new_product.price
     }
+@app.get("/health")
+def health():
+    return {"status": "ok"}
